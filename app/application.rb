546c95a2374259e53d3c851f2d592ab0934f0443
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-23 12:50:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     price = 0
+<<<<<<< HEAD
     item = req.path.split("/items/").last
     if req.path=="/items/" + item
       Item.all.each do |line|
@@ -20,6 +21,17 @@ class Application
       else
          resp.write price
       end
+=======
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      Item.all.each do |line|
+        if item == line.name
+          price = line.price
+        end
+      end
+      if price == 0 
+        
+>>>>>>> ec68b2aab5426d62079aef3dce783d41e8bfcf15
     else
       resp.write "Route not found"
       resp.status = 404
